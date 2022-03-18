e7f80e2e908722ab14ee2fe8394b1f3ebcdad2ef
curoky <cccuroky@gmail.com>
2022-03-19 01:22:43 +0800

    feat(bin): add echo

---
+++ bin/echo/echo.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Copyright (c) -2023 curoky(cccuroky@gmail.com).
+#
+# This file is part of thriftoy.
+# See https://github.com/curoky/thriftoy for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+
+import thriftpy2
+import typer
+from thriftpy2.rpc import make_client, make_server
+
+from thriftoy import ProtocolType, TransportType
+
+app = typer.Typer()
+echo_thrift = thriftpy2.load("echo.thrift", module_name="echo_thrift")
+
+
+class Dispatcher:
+    def echo(self, param):
+        # print(param)
+        return param
+
+
+@app.command()
+def server(
+    host: str = "0.0.0.0",
+    port: int = 6000,
+    protocol_type: ProtocolType = ProtocolType.BINARY,
+    transport_type: TransportType = TransportType.FRAMED,
+):
+    server = make_server(
+        echo_thrift.EchoService,
+        Dispatcher(),
+        host=host,
+        port=port,
+        trans_factory=transport_type.get_factory(),
+        proto_factory=protocol_type.get_factory(),
+    )
+    server.serve()
+
+
+@app.command()
+def client(
+    host: str = "0.0.0.0",
+    port: int = 6000,
+    protocol_type: ProtocolType = ProtocolType.BINARY,
+    transport_type: TransportType = TransportType.FRAMED,
+):
+    client = make_client(
+        echo_thrift.EchoService,
+        host=host,
+        port=port,
+        trans_factory=transport_type.get_factory(),
+        proto_factory=protocol_type.get_factory(),
+    )
+
+    print(client.echo("hello, world"))
+    client.close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+    app()
