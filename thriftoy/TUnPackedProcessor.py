51765aa05ca68612a29c530a5e45afc7f7cfe1a9
curoky <cccuroky@gmail.com>
2022-03-19 01:21:47 +0800

    feat(thriftoy): add TUnPackedProcessor

---
+++ thriftoy/TUnPackedProcessor.py
@@ -0,0 +1,46 @@
+# Copyright (c) -2023 curoky(cccuroky@gmail.com).
+#
+# This file is part of thriftoy.
+# See https://github.com/curoky/thriftoy for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from thriftpy2.protocol.binary import TBinaryProtocol
+from thriftpy2.rpc import TSocket
+
+from .ThriftMessage import ThriftMessage
+from .ThriftStruct import EmptyThriftStruct
+from .TMemoryComplexTransport import TMemoryComplexTransport
+
+
+class TUnPackedProcessor:
+    def unpack_message(self, prot: TBinaryProtocol) -> ThriftMessage:
+        method, type, seqid = prot.read_message_begin()
+        prot.read_struct(EmptyThriftStruct())
+        prot.read_message_end()
+        data = prot.trans.getvalue()
+        return ThriftMessage(method=method, type=type, seqid=seqid, data=data)
+
+    def process(self, iprot: TBinaryProtocol, oprot: TBinaryProtocol):
+        itrans: TMemoryComplexTransport = iprot.trans
+        message = self.unpack_message(iprot)
+        socket: TSocket = itrans._trans
+        assert socket.sock is not None
+        message.from_host, message.from_port = socket.sock.getpeername()
+        message.listen_host, message.listen_port = socket.sock.getsockname()
+        self.process_message(socket, message)
+
+        # NOTICE: if call `itrans.close()`, we should
+        # `raise TTransportException(TTransportException.END_OF_FILE)`
+
+    def process_message(self, socket: TSocket, message: ThriftMessage):
+        pass
