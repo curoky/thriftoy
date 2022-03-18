6d4b89604e22c74899ab83252110607147be4778
curoky <cccuroky@gmail.com>
2022-03-19 01:21:56 +0800

    feat(thriftoy): init package

---
+++ thriftoy/__init__.py
@@ -0,0 +1,46 @@
+# Copyright (c) -2023 curoky(cccuroky@gmail.com).
+#
+# This file is part of thriftoy.
+# See https://github.com/curoky/thriftoy for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from enum import Enum
+
+from thriftpy2.protocol.binary import TBinaryProtocolFactory
+from thriftpy2.protocol.compact import TCompactProtocolFactory
+from thriftpy2.transport import TBufferedTransportFactory, TFramedTransportFactory
+
+
+class ProtocolType(str, Enum):
+    BINARY = "binary"
+    COMPACT = "compact"
+
+    def get_factory(self):
+        match self.value:
+            case ProtocolType.BINARY:
+                return TBinaryProtocolFactory()
+            case ProtocolType.COMPACT:
+                return TCompactProtocolFactory()
+
+
+class TransportType(str, Enum):
+    FRAMED = "framed"
+    BUFFERED = "buffered"
+    THEADER = "theader"
+
+    def get_factory(self):
+        match self.value:
+            case TransportType.FRAMED:
+                return TFramedTransportFactory()
+            case TransportType.BUFFERED:
+                return TBufferedTransportFactory()
